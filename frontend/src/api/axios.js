36e89cb77dc16647d5bbfc967088ac5b0ce66fab
Akarsh Deva <akarshdeva90063@gmail.com>
2026-08-08 16:41:03 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://nexus-yaim.onrender.com',
 });
 
 // Attach the JWT token to every request automatically
